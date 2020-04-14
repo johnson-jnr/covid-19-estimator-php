21488e45e13c1126bcba96fdd08a93619da5325f
Johnson Towoju <johnsontodimu@gmail.com>
2020-04-14 02:56:21 +0100

    api done

--- src/estimator.php
+++ src/estimator.php
@@ -4,55 +4,53 @@
 function covid19ImpactEstimator($data) {
 
 
-	$output = array();
-	$output['data'] = $data;
+	$estimate = array();
+	$estimate['data'] = $data;
 
 	$timeToElapse				= $data['timeToElapse'];
 	$periodType 				= $data['periodType'];
-	$timeToElapse 				= getDaysToElapse( $timeToElapse, $periodType );
+	$timeToElapse 			= getDaysToElapse( $timeToElapse, $periodType );
 
-	//Challenge 1
-	$currentlyInfected 				= $data['reportedCases'] * 10;
-	$currentlyInfected_severely 			= $data['reportedCases'] * 50;
-	$setOfDays 					= getSetNumberOfDays( $timeToElapse );
-	$infectionsByRequestedTime 			= $currentlyInfected * ( 2 ** $setOfDays );
+	$currentlyInfected 											= $data['reportedCases'] * 10;
+	$currentlyInfected_severely 						= $data['reportedCases'] * 50;
+	$setOfDays 															= getSetNumberOfDays( $timeToElapse );
+	$infectionsByRequestedTime 							= $currentlyInfected * ( 2 ** $setOfDays );
 	$infectionsByRequestedTime_severely 		= $currentlyInfected_severely * ( 2 ** $setOfDays ); 
 
-	//Challenge 2
-	$severeCasesByRequestedTime 		 	= 0.15 * $infectionsByRequestedTime;
+	$severeCasesByRequestedTime 		 				= 0.15 * $infectionsByRequestedTime;
 	$severeCasesByRequestedTime_severely 		= 0.15 * $infectionsByRequestedTime_severely;
-	$totalHospitalBeds 				= 0.35 * $data['totalHospitalBeds'];
-	$hospitalBedsByRequestedTime 			= $totalHospitalBeds - $severeCasesByRequestedTime;
-	$hospitalBedsByRequestedTime_severely 		= $totalHospitalBeds - $severeCasesByRequestedTime_severely;
+	$totalHospitalBeds 											= 0.35 * $data['totalHospitalBeds'];
+	$hospitalBedsByRequestedTime 						= $totalHospitalBeds - $severeCasesByRequestedTime;
+	$hospitalBedsByRequestedTime_severely 	= $totalHospitalBeds - $severeCasesByRequestedTime_severely;
 
-	//Challenge 3
-	$casesForICUByRequestedTime 			= 0.05 * $infectionsByRequestedTime;
-	$casesForICUByRequestedTime_severely 		= 0.05 * $infectionsByRequestedTime_severely;
-	$casesForVentilatorsByRequestedTime 		= 0.02 * $infectionsByRequestedTime;
+	$casesForICUByRequestedTime 									= 0.05 * $infectionsByRequestedTime;
+	$casesForICUByRequestedTime_severely 					= 0.05 * $infectionsByRequestedTime_severely;
+	$casesForVentilatorsByRequestedTime 					= 0.02 * $infectionsByRequestedTime;
 	$casesForVentilatorsByRequestedTime_severely	= 0.02 * $infectionsByRequestedTime_severely;
-	$avgDailyIncomeInUSD 		= $data['region']['avgDailyIncomeInUSD'];
-	$avgDailyIncomePopulation 	= $data['region']['avgDailyIncomePopulation'];
-	$dollarsInFlight 		= ($infectionsByRequestedTime * $avgDailyIncomeInUSD * $avgDailyIncomePopulation) / $timeToElapse;
-	$dollarsInFlight_severely	= ($infectionsByRequestedTime_severely * $avgDailyIncomeInUSD * $avgDailyIncomePopulation) / $timeToElapse;
+	$avgDailyIncomeInUSD 													= $data['region']['avgDailyIncomeInUSD'];
+	$avgDailyIncomePopulation 										= $data['region']['avgDailyIncomePopulation'];
+	$dollarsInFlight 															= ($infectionsByRequestedTime * $avgDailyIncomeInUSD * $avgDailyIncomePopulation) / $timeToElapse;
+	$dollarsInFlight_severely											= ($infectionsByRequestedTime_severely * $avgDailyIncomeInUSD * $avgDailyIncomePopulation) 
+																										/ $timeToElapse;
 
 
-	$output['impact']['currentlyInfected'] 		= (int) $currentlyInfected;
-	$output['severeImpact']['currentlyInfected'] 		= (int) $currentlyInfected_severely;
-	$output['impact']['infectionsByRequestedTime'] 		= (int) $infectionsByRequestedTime;
-	$output['severeImpact']['infectionsByRequestedTime'] 	= (int) $infectionsByRequestedTime_severely;
-	$output['impact']['severeCasesByRequestedTime'] 	= (int) $severeCasesByRequestedTime;
-	$output['severeImpact']['severeCasesByRequestedTime'] 	= (int) $severeCasesByRequestedTime_severely;
-	$output['impact']['hospitalBedsByRequestedTime'] 	= (int) $hospitalBedsByRequestedTime;
-	$output['severeImpact']['hospitalBedsByRequestedTime']		= (int) $hospitalBedsByRequestedTime_severely;
+	$estimate['impact']['currentlyInfected'] 												= (int) $currentlyInfected;
+	$estimate['severeImpact']['currentlyInfected'] 									= (int) $currentlyInfected_severely;
+	$estimate['impact']['infectionsByRequestedTime'] 								= (int) $infectionsByRequestedTime;
+	$estimate['severeImpact']['infectionsByRequestedTime'] 					= (int) $infectionsByRequestedTime_severely;
+	$estimate['impact']['severeCasesByRequestedTime'] 							= (int) $severeCasesByRequestedTime;
+	$estimate['severeImpact']['severeCasesByRequestedTime'] 				= (int) $severeCasesByRequestedTime_severely;
+	$estimate['impact']['hospitalBedsByRequestedTime'] 							= (int) $hospitalBedsByRequestedTime;
+	$estimate['severeImpact']['hospitalBedsByRequestedTime']				= (int) $hospitalBedsByRequestedTime_severely;
 
-	$output['impact']['casesForICUByRequestedTime']		= (int) $casesForICUByRequestedTime;
-	$output['severeImpact']['casesForICUByRequestedTime'] 	= (int) $casesForICUByRequestedTime_severely;
-	$output['impact']['casesForVentilatorsByRequestedTime']	= (int) $casesForVentilatorsByRequestedTime;
-	$output['severeImpact']['casesForVentilatorsByRequestedTime']	= (int) $casesForVentilatorsByRequestedTime_severely;
-	$output['impact']['dollarsInFlight']				= (int) $dollarsInFlight;
-	$output['severeImpact']['dollarsInFlight']			= (int) $dollarsInFlight_severely;
+	$estimate['impact']['casesForICUByRequestedTime']								= (int) $casesForICUByRequestedTime;
+	$estimate['severeImpact']['casesForICUByRequestedTime'] 				= (int) $casesForICUByRequestedTime_severely;
+	$estimate['impact']['casesForVentilatorsByRequestedTime']				= (int) $casesForVentilatorsByRequestedTime;
+	$estimate['severeImpact']['casesForVentilatorsByRequestedTime']	= (int) $casesForVentilatorsByRequestedTime_severely;
+	$estimate['impact']['dollarsInFlight']													= (int) $dollarsInFlight;
+	$estimate['severeImpact']['dollarsInFlight']										= (int) $dollarsInFlight_severely;
 
-	return $output;
+	return $estimate;
 }
 
 
@@ -83,31 +81,34 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-$uri_path = array_filter(explode( '/', $uri ));
-$content_type = end($uri_path);
-$requestMethod = $_SERVER["REQUEST_METHOD"];
+$uri_path 			= array_filter(explode( '/', $uri ));
+$content_type 	= end($uri_path);
+$requestMethod 	= $_SERVER["REQUEST_METHOD"];
+
+
+// Endpoint start with /api else results in a 404 Not Found
+if ($uri_path[1] !== 'api') {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
 
 
 $time_pre_exec = microtime(true);
-
 if ( $content_type == 'xml' ) {
 
 	header("Content-Type: application/xml; charset=UTF-8");
-
 	$input = trim(file_get_contents("PHP://input"));
 	$input = json_decode($input, true);
 
 	$estimate = covid19ImpactEstimator($input);
-	$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><data/>');
+	$xml 			= new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><data/>');
 	to_xml($xml, $estimate);
 	$result = $xml->asXML();
 
 	$response['status_code_header'] = 'HTTP/1.1 200 OK';
 	$response['body'] =  $result;
 	return $response
-		// $response['body'] = $var;
-		// echo $response['body'];
-
+	
 } elseif ( $content_type == 'logs' ) {
 
 	header("Content-Type: text/plain; charset=UTF-8");
@@ -125,19 +126,13 @@ if ( $content_type == 'xml' ) {
 	$input = trim(file_get_contents("PHP://input"));
 	$input = json_decode($input, true);
 
-	$estimate = covid19ImpactEstimator($input);
+	$estimate	= covid19ImpactEstimator($input);
 	$response['status_code_header'] = 'HTTP/1.1 200 OK';
 	$response['body'] = json_encode($estimate);
 	return $response;
-
-
-		// $response['body'] = json_encode($result);
-		// echo $response['body'];
-
 }
 
 logRequest( $requestMethod, $uri, $time_pre );
-
 
 
 function to_xml(SimpleXMLElement $object, array $data) {   
@@ -163,16 +158,16 @@ function logRequest( $requestMethod, $uri, $time_pre ) {
 	$responseCode = http_response_code(); 
 
 	$time_post_exec = microtime(true);
-	$exec_time = $time_post_exec - $time_pre_exec;
-	$exec_time = strtok($exec_time, ".");
-	$length = strlen($exec_time);
+	$exec_time 			= $time_post_exec - $time_pre_exec;
+	$exec_time 			= strtok($exec_time, ".");
+	$length 				= strlen($exec_time);
 
 	if ($length < 2) {
 		$exec_time = '0' . $exec_time;
 	}
 
-	$txt = "$requestMethod" . "\t\t" . "$uri" .  "\t\t" . $responseCode . "\t\t" . $exec_time . 'ms';
-	$myfile = file_put_contents( 'logs.txt', $txt.PHP_EOL , FILE_APPEND );
+	$log_txt = "$requestMethod" . "\t\t" . "$uri" .  "\t\t" . $responseCode . "\t\t" . $exec_time . 'ms';
+	file_put_contents( 'logs.txt', $log_txt.PHP_EOL , FILE_APPEND );
 
 }
 
