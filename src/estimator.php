2e42a98445fd9a13d36ba3b116513044d16efa8d
Johnson Towoju <johnsontodimu@gmail.com>
2020-04-14 01:36:59 +0100

    init

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,8 @@
 <?php
 
+
 function covid19ImpactEstimator($data) {
+
 
 	$output = array();
 	$output['data'] = $data;
@@ -16,14 +18,41 @@ function covid19ImpactEstimator($data) {
 	$infectionsByRequestedTime 			= $currentlyInfected * ( 2 ** $setOfDays );
 	$infectionsByRequestedTime_severely 		= $currentlyInfected_severely * ( 2 ** $setOfDays ); 
 
+	//Challenge 2
+	$severeCasesByRequestedTime 		 	= 0.15 * $infectionsByRequestedTime;
+	$severeCasesByRequestedTime_severely 		= 0.15 * $infectionsByRequestedTime_severely;
+	$totalHospitalBeds 				= 0.35 * $data['totalHospitalBeds'];
+	$hospitalBedsByRequestedTime 			= $totalHospitalBeds - $severeCasesByRequestedTime;
+	$hospitalBedsByRequestedTime_severely 		= $totalHospitalBeds - $severeCasesByRequestedTime_severely;
+
+	//Challenge 3
+	$casesForICUByRequestedTime 			= 0.05 * $infectionsByRequestedTime;
+	$casesForICUByRequestedTime_severely 		= 0.05 * $infectionsByRequestedTime_severely;
+	$casesForVentilatorsByRequestedTime 		= 0.02 * $infectionsByRequestedTime;
+	$casesForVentilatorsByRequestedTime_severely	= 0.02 * $infectionsByRequestedTime_severely;
+	$avgDailyIncomeInUSD 		= $data['region']['avgDailyIncomeInUSD'];
+	$avgDailyIncomePopulation 	= $data['region']['avgDailyIncomePopulation'];
+	$dollarsInFlight 		= ($infectionsByRequestedTime * $avgDailyIncomeInUSD * $avgDailyIncomePopulation) / $timeToElapse;
+	$dollarsInFlight_severely	= ($infectionsByRequestedTime_severely * $avgDailyIncomeInUSD * $avgDailyIncomePopulation) / $timeToElapse;
 
 
 	$output['impact']['currentlyInfected'] 		= (int) $currentlyInfected;
 	$output['severeImpact']['currentlyInfected'] 		= (int) $currentlyInfected_severely;
 	$output['impact']['infectionsByRequestedTime'] 		= (int) $infectionsByRequestedTime;
-	$output['severeImpact']['infectionsByRequestedTime'] 	= (int) $infectionsByRequestedTime_severely;	
+	$output['severeImpact']['infectionsByRequestedTime'] 	= (int) $infectionsByRequestedTime_severely;
+	$output['impact']['severeCasesByRequestedTime'] 	= (int) $severeCasesByRequestedTime;
+	$output['severeImpact']['severeCasesByRequestedTime'] 	= (int) $severeCasesByRequestedTime_severely;
+	$output['impact']['hospitalBedsByRequestedTime'] 	= (int) $hospitalBedsByRequestedTime;
+	$output['severeImpact']['hospitalBedsByRequestedTime']		= (int) $hospitalBedsByRequestedTime_severely;
 
-	return $data;
+	$output['impact']['casesForICUByRequestedTime']		= (int) $casesForICUByRequestedTime;
+	$output['severeImpact']['casesForICUByRequestedTime'] 	= (int) $casesForICUByRequestedTime_severely;
+	$output['impact']['casesForVentilatorsByRequestedTime']	= (int) $casesForVentilatorsByRequestedTime;
+	$output['severeImpact']['casesForVentilatorsByRequestedTime']	= (int) $casesForVentilatorsByRequestedTime_severely;
+	$output['impact']['dollarsInFlight']				= (int) $dollarsInFlight;
+	$output['severeImpact']['dollarsInFlight']			= (int) $dollarsInFlight_severely;
+
+	return $output;
 }
 
 
@@ -45,3 +74,5 @@ function getDaysToElapse($timeToElapse, $periodType) {
 
 	return $timeToElapse;
 }
+
+
