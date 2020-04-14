19f5a0354e6f86a1110ca95dba613eee0ab65f51
Johnson Towoju <johnsontodimu@gmail.com>
2020-04-14 01:26:02 +0100

    init

--- src/estimator.php
+++ src/estimator.php
@@ -2,6 +2,46 @@
 
 function covid19ImpactEstimator($data) {
 
-	echo 'hope to get it right';
+	$output = array();
+	$output['data'] = $data;
+
+	$timeToElapse				= $data['timeToElapse'];
+	$periodType 				= $data['periodType'];
+	$timeToElapse 				= getDaysToElapse( $timeToElapse, $periodType );
+
+	//Challenge 1
+	$currentlyInfected 				= $data['reportedCases'] * 10;
+	$currentlyInfected_severely 			= $data['reportedCases'] * 50;
+	$setOfDays 					= getSetNumberOfDays( $timeToElapse );
+	$infectionsByRequestedTime 			= $currentlyInfected * ( 2 ** $setOfDays );
+	$infectionsByRequestedTime_severely 		= $currentlyInfected_severely * ( 2 ** $setOfDays ); 
+
+
+
+	$output['impact']['currentlyInfected'] 		= (int) $currentlyInfected;
+	$output['severeImpact']['currentlyInfected'] 		= (int) $currentlyInfected_severely;
+	$output['impact']['infectionsByRequestedTime'] 		= (int) $infectionsByRequestedTime;
+	$output['severeImpact']['infectionsByRequestedTime'] 	= (int) $infectionsByRequestedTime_severely;	
+
 	return $data;
+}
+
+
+function getSetNumberOfDays( $timeToElapse ) {
+
+	$setOfDays = $timeToElapse / 3;
+	$setOfDays = (int) $setOfDays;
+
+	return $setOfDays;
+}
+
+function getDaysToElapse($timeToElapse, $periodType) {
+
+	if ($periodType == 'weeks') {
+		$timeToElapse = $timeToElapse * 7;
+	} elseif ($periodType == 'months') {
+		$timeToElapse = $timeToElapse * 30;
+	} 
+
+	return $timeToElapse;
 }
